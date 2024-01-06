6cc17cc5feffa127b4a5a105ac30fa97aa4f2b68
Fenhl <fenhl@fenhl.net>
2024-01-06 15:51:00 +0000

    Remove errors module

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
-pub use crate::{
-    data::{
-        Chaser,
-        Line,
-    },
-    errors::Error,
+use tokio::{
+    io,
+    sync::mpsc,
+};
+pub use crate::data::{
+    Chaser,
+    Line,
 };
 
 mod r#async;
 mod data;
-mod errors;
 mod sync;
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)] Io(#[from] io::Error),
+    #[error(transparent)] Send(#[from] mpsc::error::SendError<String>),
+    #[error(transparent)] Custom(Box<dyn std::error::Error + Send + Sync>),
+}
