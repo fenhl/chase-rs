88120ef6900edff2dbe8f25d022c2c78462fb7d8
Fenhl <fenhl@fenhl.net>
2024-01-06 15:46:51 +0000

    Remove unused error variant

--- src/errors.rs
+++ src/errors.rs
@@ -1,15 +1,11 @@
-use {
-    std::{
-        io,
-        sync::mpsc as channel_mpsc,
-    },
-    tokio::sync::mpsc as stream_mpsc,
+use tokio::{
+    io,
+    sync::mpsc,
 };
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error(transparent)] IoError(#[from] io::Error),
-    #[error(transparent)] ChannelSendError(#[from] channel_mpsc::SendError<String>),
-    #[error(transparent)] StreamSendError(#[from] stream_mpsc::error::SendError<String>),
+    #[error(transparent)] Io(#[from] io::Error),
+    #[error(transparent)] Send(#[from] mpsc::error::SendError<String>),
     #[error(transparent)] Custom(Box<dyn std::error::Error + Send + Sync>),
 }
